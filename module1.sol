678b30c3ed125c905b5fb09fc89389ff00361334
sukant kumar <129366933+shresth19@users.noreply.github.com>
2023-08-31 23:03:08 +0530

    Update module1.sol

--- module1.sol
+++ module1.sol
@@ -10,16 +10,16 @@ contract LotteryExample {
         owner = msg.sender;
     }
 
-    function joinLottery() public {
+    function joinLottery() public payable {
 
         require(msg.sender != owner, "Owner cannot join the lottery");
         participants.push(msg.sender);
 
-    //we need atleast 2 member for the lottery to happen
+    // we need atleast 2 member for the lottery to happen
         assert(participants.length > 1);
 
-    //revert statements prevents additional participants to join once the lottery is full
-        if (participants.length > 10) {
+    // revert statements prevents additional participants to join once the lottery is full
+        if (participants.length > 2) {
             revert("Lottery is full");
         }
     }
